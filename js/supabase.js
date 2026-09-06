bdec657a323eaf72a09c5209824f8e5bc6641e1b
Marcblack08 <137033747+Marcblack08@users.noreply.github.com>
2026-09-05 21:36:36 -0500

    Initialize Supabase client with URL and key

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://mcdreqnacuoyqngvcysq.supabase.co";
+const SUPABASE_KEY = "sb_publishable_rOfcfSoPFYDk111_eFLJNQ_MugVpDO5";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
